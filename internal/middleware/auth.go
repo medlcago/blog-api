693bc2d5f17b1950675f5c82e897c12ab756c261
alexander <alex.korolev99@vk.com>
2025-08-31 20:14:14 +0300

    AuthMiddleware: fix logger mutation

--- internal/middleware/auth.go
+++ internal/middleware/auth.go
@@ -18,30 +18,28 @@ func (m *Manager) AuthMiddleware() fiber.Handler {
 
 	return func(ctx fiber.Ctx) error {
 		requestID := requestid.FromContext(ctx)
-		log = log.With(
-			slog.String(string(logger.RequestIDKey), requestID),
-		)
+		reqLog := log.With(slog.String(string(logger.RequestIDKey), requestID))
 
 		tokenHeader := fiber.GetReqHeader[string](ctx, "Authorization")
 		if len(tokenHeader) < 7 || !strings.EqualFold(tokenHeader[:7], "bearer ") {
-			log.Info("missing bearer token")
+			reqLog.Info("missing bearer token")
 			return errors.ErrMissingToken
 		}
 
 		claims, err := m.jwtService.ValidateToken(tokenHeader[7:])
 		if err != nil {
-			log.Info("invalid token")
+			reqLog.Info("invalid token")
 			return errors.ErrUnauthorized
 		}
 
 		if claims.TokenType != tokenmanager.AccessToken {
-			log.Info("invalid token type", slog.String("token_type", claims.TokenType))
+			reqLog.Info("invalid token type", slog.String("token_type", claims.TokenType))
 			return errors.ErrUnauthorized
 		}
 
 		userID, err := strconv.ParseUint(claims.UserID, 10, 64)
 		if err != nil {
-			log.Error("invalid user id", slog.Any("error", err))
+			reqLog.Error("invalid user id", slog.Any("error", err))
 			return errors.ErrUnauthorized
 		}
 
